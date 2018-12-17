65e8c6867bf648e53c810174b60b3deb5b1dc4aa
SriramPingali <43778095+SriramPingali@users.noreply.github.com>
2018-12-17 10:45:42 +0530

    Update calculates_results_stats.py
    Change the format of numbers from string to integers.

--- Dog_image_classifier/Dog_image_classifier.tar/Dog_image_classifier/calculates_results_stats.py
+++ Dog_image_classifier/Dog_image_classifier.tar/Dog_image_classifier/calculates_results_stats.py
@@ -97,13 +97,13 @@ def calculates_results_stats(results_dic):
             results_stats_dic['n_dogs_img']+=1
         if labels[3]==0:
             results_stats_dic['n_notdogs_img']+=1
-        if labels[2]=='1':
+        if labels[2]==1:
             results_stats_dic['n_match']+=1
         if labels[4]==1:
             results_stats_dic['n_correct_dogs']+=1
         if labels[4]==0:
              results_stats_dic['n_correct_notdogs']+=1
-        if labels[2]=='1' and labels[3]==1:
+        if labels[2]==1 and labels[3]==1:
             results_stats_dic['n_correct_breed']+=1
     results_stats_dic['pct_match']=results_stats_dic['n_match']/results_stats_dic['n_images']*100 
     results_stats_dic['pct_correct_dogs']=results_stats_dic['n_correct_dogs']/results_stats_dic['n_images']*100   
